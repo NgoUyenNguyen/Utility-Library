3884bc08a262d7832e40ef41689ac957d3f91eb3
NgoUyenNguyen <nguyenphucnguyenlhp@gmail.com>
2026-03-03 00:04:45 +0700

    Add EditorSettings and EditorSettingsProvider for improved settings management

---
+++ Editor/Scripts/EditorSettings/EditorSettings.cs
@@ -0,0 +1,36 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace NgoUyenNguyen.Editor
+{
+    public class EditorSettings : ScriptableObject
+    {
+        [HideInInspector]
+        public bool sceneInitializeEnable;
+        [HideInInspector]
+        public SceneAsset sceneInitializeSceneAsset;
+        
+        [HideInInspector]
+        public LevelEditorSettings levelEditorSettings;
+        
+        
+        
+        public static EditorSettings GetOrCreate()
+        {
+            const string settingsPath = "Assets/Settings/NgoUyenNguyen.EditorSettings.asset";
+            var settings = AssetDatabase.LoadAssetAtPath<EditorSettings>(settingsPath);
+            
+            // ReSharper disable once InvertIf
+            if (settings == null)
+            {
+                settings = CreateInstance<EditorSettings>();
+                if (!AssetDatabase.IsValidFolder("Assets/Editor"))
+                    AssetDatabase.CreateFolder("Assets", "Editor");
+                
+                AssetDatabase.CreateAsset(settings, settingsPath);
+                AssetDatabase.ImportAsset(settingsPath);
+            }
+            return settings;
+        }
+    }
+}
